8db01b9a3003e9e5423c441979e43baa13c9afda
Gary Kong <garygarylmao@gmail.com>
2023-02-27 22:37:31 +0800

    sync

--- public/js/gameplay.js
+++ public/js/gameplay.js
@@ -4,7 +4,7 @@ import { pose, onResults, camera } from "/js/mediapipe.js"
 $(window).on('load', async function () {
     await pose.onResults(onResults);
     await camera.start();
-    animate()
+    // animate()
     $(".loader").fadeOut();
     $("#preloder").delay(400).fadeOut("slow");
 });
@@ -26,4 +26,52 @@ function animate() {
 
     console.log("FPS: " + currentFPS)
     window.requestAnimationFrame(animate);
+}
+
+// Start Menu
+const playBtn = document.querySelector(".play-btn")
+const startMenu = document.querySelector("#start-menu")
+const fadeElm = document.querySelector('.fade-text')
+const homeBtn = document.querySelector('.home-btn')
+
+playBtn.addEventListener("click", playerReady)
+homeBtn.addEventListener('click', home)
+
+function startGame() {
+    console.log("Run Game Lor")
+    // Delay the display none
+    startMenu.style.display = 'none'
+    // Start the video in upcoming seconds
+    // run the mediapipe calculation
+}
+
+async function playerReady() {
+    fadeElm.style.display = "block"
+    playBtn.style.display = "none"
+    await fadeOut(fadeElm)
+    startGame()
+    // startMenu.style.display = 'none'
+}
+
+async function fadeOut(element) {
+    // Amend the fade-out effect
+    await setTimeout(function () {
+        element.classList.toggle('fade')
+        element.style.display = "none";
+        // element.classList.add("fade-out");
+    }, 3000)
+}
+
+function home() {
+    console.log("Home");
+}
+
+
+// Pause Function
+let isPaused = false;
+
+
+function pauseGame() {
+    isPaused = true;
+
 }
